ecfec8a31508ff7590079f049f4742b47f3bf0f4
Sasukexo <aswinash1002@gmail.com>
2023-08-11 20:03:42 +0530

    header poda maranten bro

---
+++ index.js
@@ -0,0 +1,53 @@
+var $background = document.getElementById('demo-background');
+var $backgroundAlpha = document.getElementById('demo-background-alpha');
+var $foreground = document.getElementById('demo-foreground');
+var $foregroundAlpha = document.getElementById('demo-foreground-alpha');
+var $level = document.getElementById('demo-level');
+var $padding = document.getElementById('demo-padding');
+var $size = document.getElementById('demo-size');
+var $value = document.getElementById('demo-value');
+
+var qr = window.qr = new QRious({
+  element: document.getElementById('qrious'),
+  size: 250,
+  value: 'QRious'
+});
+
+$background.addEventListener('change', function() {
+  qr.background = $background.value || null;
+});
+
+$backgroundAlpha.addEventListener('change', function() {
+  qr.backgroundAlpha = $backgroundAlpha.value || null;
+});
+
+$foreground.addEventListener('change', function() {
+  qr.foreground = $foreground.value || null;
+});
+
+$foregroundAlpha.addEventListener('change', function() {
+  qr.foregroundAlpha = $foregroundAlpha.value || null;
+});
+
+$level.addEventListener('change', function() {
+  qr.level = $level.value;
+});
+
+$padding.addEventListener('change', function() {
+  if ($padding.validity.valid) {
+    qr.padding = $padding.value !== '' ? $padding.value : null;
+  }
+});
+
+$size.addEventListener('change', function() {
+  if (!$size.validity.valid) {
+    return;
+  }
+
+  qr.size = $size.value || null;
+});
+
+$value.value = 'QRious';
+$value.addEventListener('input', function() {
+  qr.value = $value.value;
+});
